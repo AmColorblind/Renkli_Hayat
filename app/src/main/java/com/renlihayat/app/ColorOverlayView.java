dfbc0083ecb7111f885dcd78a0fcb108db06e0b9
Hasan çolba <136272586+AmColorblind@users.noreply.github.com>
2026-03-06 18:00:42 +0200

    Create ColorOverlayView.java

---
+++ app/src/main/java/com/renlihayat/app/ColorOverlayView.java
@@ -0,0 +1,27 @@
+package com.renlihayat.app;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+
+public class ColorOverlayView extends View {
+
+    private Paint overlayPaint;
+
+    public ColorOverlayView(Context context) {
+        super(context);
+        overlayPaint = new Paint();
+        setLayerType(LAYER_TYPE_HARDWARE, overlayPaint);
+    }
+
+    public void setPaint(Paint paint) {
+        this.overlayPaint = paint;
+        setLayerType(LAYER_TYPE_HARDWARE, overlayPaint);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPaint(overlayPaint);
+    }
+}
